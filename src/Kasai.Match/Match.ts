a920b4fbdd6f5cdfd0ded27cfb2ee2e58ac5a5bb
cshepp <me@codyshepp.com>
2015-09-20 09:23:21 -0400

    added a guard clause to allow for returning values instead of lambda expressions

--- src/Kasai.Match/Match.ts
+++ src/Kasai.Match/Match.ts
@@ -4,7 +4,13 @@ import {isMatch} from '../Kasai.Compare/Compare'
 export var match = (value: any, patterns: any[]) => {
 
     patterns.forEach((pattern) => {
+
         let [guard, result] = pattern
+
+        if(typeof result !== 'function'){
+            result = (x) => result
+        }
+
         if(isMatch(value, guard)) return result(value);
-    });
+    })
 }
