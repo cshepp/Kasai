d86f25aeee07a3f55e0625e7c432f40e616b577e
cshepp <me@codyshepp.com>
2015-09-20 09:23:57 -0400

    export typeOf and instanceOf functions from top-level module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 
 import {match as _match} from './Kasai.Match/Match'
-import {_ as x} from './Kasai.Compare/Compare'
+import {_ as x, typeOf as _typeOf, instanceOf as _instanceOf} from './Kasai.Compare/Helpers'
 
 export var match = _match
-export var _ = x;
+export var _ = x
+export var typeOf = _typeOf
+export var instanceOf = _instanceOf
