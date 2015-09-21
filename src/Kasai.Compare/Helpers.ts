8a2f4050332bd1a47af8c474b60279652f4b9078
cshepp <me@codyshepp.com>
2015-09-20 21:13:17 -0400

    specify string type for typeOf helper

--- src/Kasai.Compare/Helpers.ts
+++ src/Kasai.Compare/Helpers.ts
@@ -1,6 +1,6 @@
 
 export var _ = (x) => true
 
-export var typeOf = (t) => (x) => typeof x === t
+export var typeOf = (t: string) => (x) => typeof x === t
 
 export var instanceOf = (c) => (x) => x instanceof c
