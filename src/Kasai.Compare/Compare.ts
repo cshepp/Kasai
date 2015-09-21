7a1189d2de89275901b76ec181376ba249e244f5
cshepp <me@codyshepp.com>
2015-09-20 21:14:30 -0400

    Implemented comparison functions and tests

--- src/Kasai.Compare/Compare.ts
+++ src/Kasai.Compare/Compare.ts
@@ -1,29 +1,44 @@
 
 export var compareArrays = (value: any, pattern: any): boolean => {
+
+    if (value.length !== pattern.length) return false
+
     var correct = value.map((x, i) => {
         return isMatch(x, pattern[i]) ? 1 : 0
     })
 
-    var numCorrect = correct.reduce((p, c) => p + c, 0);
-    return numCorrect === value.length;
+    var numCorrect = correct.reduce((p, c) => p + c, 0)
+    return numCorrect === value.length
 }
 
 export var compareObjects = (value: any, pattern: any): boolean => {
-    return true
+    
+    var valueKeys = Object.keys(value).filter((x) => value.hasOwnProperty(x))
+    var patternKeys = Object.keys(pattern).filter((x) => pattern.hasOwnProperty(x))
+    
+    var orphanKeys = patternKeys.filter((k) => valueKeys.indexOf(k) === -1)
+    if (orphanKeys.length > 0) return false
+
+    var correct = patternKeys.map((k) => {         
+        return isMatch(value[k], pattern[k]) ? 1 : 0
+    })
+    
+    var numCorrect = correct.reduce((p, c) => p + c, 0)
+    return numCorrect === patternKeys.length
 }
 
 export var getComparator = (pattern: any): Function => {
+    
+    if (typeof pattern === 'function') return pattern
+    if (typeof pattern === 'boolean') return (x) => x === pattern
+    if (typeof pattern === 'number') return (x) => x === pattern
+    if (typeof pattern === 'string') return (x) => x === pattern
+    if (typeof pattern === 'object' && pattern.splice !== undefined) return (x) => compareArrays(x, pattern)
 
-    if(typeof pattern === 'function') return pattern
-    if(typeof pattern === 'boolean') return (x) => x === pattern
-    if(typeof pattern === 'string') return (x) => x === x
-    if(typeof pattern === 'object' && pattern.splice !== undefined) return compareArrays
-
-    return compareObjects
+    return (x) => compareObjects(x, pattern)
 }
 
 export var isMatch = (value: any, pattern: any): boolean => {
-
-    var comparator = getComparator(pattern);
-    return comparator(value);
+    var comparator = getComparator(pattern)
+    return comparator(value)
 }
--- src/Kasai.Tests/Kasai.Tests.Compare/CompareTests.ts
+++ src/Kasai.Tests/Kasai.Tests.Compare/CompareTests.ts
@@ -1,14 +1,91 @@
 
 import * as tsUnit from '../../../node_modules/tsunit.external/tsUnit'
-import {compareArrays} from '../../Kasai.Compare/Compare'
+import {compareArrays, compareObjects, isMatch} from '../../Kasai.Compare/Compare'
 
 export class CompareTests extends tsUnit.TestClass {
-
-    compareArrays_ShouldReturnTrue_WhenGivenArraysThatAreEqual(){
-        this.areIdentical(true, compareArrays([0, 1], [0, 1]))
+    
+    /**
+     * Tests for compareArrays
+     */
+    compareArrays_ShouldReturnTrue_WhenGivenArraysThatAreEqual() {
+        this.isTrue(compareArrays([0, 1], [0, 1]))
     }
 
-    compareArrays_ShouldReturnFalse_WhenGivenArraysThatAreNotEqual(){
-        this.areIdentical(false, compareArrays([0, 1], ["a", "b"]))
+    compareArrays_ShouldReturnFalse_WhenGivenArraysThatAreNotEqual() {
+        this.isFalse(compareArrays([0, 1], ["a", "b"]))
+    }
+
+    compareArrays_ShouldReturnFalse_WhenValueIsShorterThanPattern() {
+        this.isFalse(compareArrays([0], [0, 1]))
+    }
+
+    compareArrays_ShouldReturnFalse_WhenArraysAreNotTheSameLength() {
+        this.isFalse(compareArrays([0, 1, 2], [0, 1]))
+    }
+
+    compareArrays_ShouldReturnFalse_WhenElementsAreSwitched() {
+        this.isFalse(compareArrays([0, 1], [1, 0]))
+    }
+    
+    /**
+     * Tests for compareObjects
+    */
+    compareObjects_ShouldReturnFalse_WhenValueDoesntIncludeKeyFromPattern() {
+        this.isFalse(compareObjects({ a: 1, c: 3 }, { a: 1, b: 2 }))
+    }
+
+    compareObjects_ShouldReturnFalse_WhenValuesAtAKeyAreDifferent() {
+        this.isFalse(compareObjects({ a: 1 }, { a: 2 }))
+    }
+
+    compareObject_ShouldReturnTrue_WhenObjectsAreEqual() {
+        this.isTrue(compareObjects({ a: 1, b: 2 }, { a: 1, b: 2 }))
+    }
+
+    compareObject_ShouldReturnTrue_WhenObjectsAreEqualAreKeysAreOutOfOrder() {
+        this.isTrue(compareObjects({ a: 1, b: 2 }, { b: 2, a: 1 }))
+    }
+    
+    /**
+     * Tests for isMatch 
+     */
+    isMatch_ShouldReturnTrue_WhenPassedEqualArrays() { 
+        this.isTrue(isMatch([1, 2], [1, 2]))
+    }
+
+    isMatch_ShouldReturnFalse_WhenPassedDifferingArrays() { 
+        this.isFalse(isMatch([1, 0, 2], [2, 6]))
+    }
+
+    isMatch_ShouldReturnTrue_WhenPassedEqualObjects() { 
+        this.isTrue(isMatch({a: 4, b: false}, {a: 4, b: false}))
+    }
+
+    isMatch_ShouldReturnFalse_WhenPassedDifferingObjects() {
+        this.isFalse(isMatch({a: 0}, {x: 0}))
+    }
+
+    isMatch_ShouldReturnTrue_WhenPassedEqualStrings() { 
+        this.isTrue(isMatch("finn", "finn"))
+    }
+
+    isMatch_ShouldReturnFalse_WhenPassedDifferingStrings() { 
+        this.isFalse(isMatch("jake", "princess bubblegum"))
+    }
+
+    isMatch_ShouldReturnTrue_WhenPassedEqualNumbers() {
+        this.isTrue(isMatch(8675309, 8675309))
+    }
+
+    isMatch_ShouldReturnFalse_WhenPassedDifferingNumbers() { 
+        this.isFalse(isMatch(0, 1234))
+    }
+
+    isMatch_ShouldReturnTrue_WhenPassedEqualBooleans() { 
+        this.isTrue(isMatch(true, true))
+    }
+
+    isMatch_ShouldReturnFalse_WhenPassedDifferingBooleans() { 
+        this.isFalse(isMatch(false, true))
     }
 }
