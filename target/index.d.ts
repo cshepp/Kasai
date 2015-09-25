e101b1d84e4dad140d78ab408f7a96b41f6946c7
cshepp <me@codyshepp.com>
2015-09-25 17:51:33 -0400

    Fix ambient module declaration

--- target/index.d.ts
+++ target/index.d.ts
@@ -1,4 +1,4 @@
-export declare module "Kasai" {
+declare module "Kasai" {
     var match: (value: any, patterns: any[]) => any;
     var _: (x: any) => boolean;
     var typeOf: (t: string) => (x: any) => boolean;
