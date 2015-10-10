26a75a7791869e427b17ff75c04772c459aac5fa
Cody A Shepp <me@codyshepp.com>
2015-10-10 10:09:21 -0400

    Add $ Capture function to top-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 
 import {match as _match} from './Kasai.Match/Match'
 import {_ as x, typeOf as _typeOf, instanceOf as _instanceOf} from './Kasai.Compare/Helpers'
+import {$ as y} from './Kasai.Compare/Capture' 
 
 export var match = _match
 export var _ = x
 export var typeOf = _typeOf
 export var instanceOf = _instanceOf
+export var $ = y
