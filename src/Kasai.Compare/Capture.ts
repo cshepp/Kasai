198de51ca008fad386fbef10213f5c6907650ace
Cody A Shepp <me@codyshepp.com>
2015-10-10 10:09:08 -0400

    Clean up Capture refactor

--- src/Kasai.Compare/Capture.ts
+++ src/Kasai.Compare/Capture.ts
@@ -5,47 +5,27 @@ export class Capture {
 
 	constructor(value: any) {
 
-		let x = []
+		let arr = []
 
-		// if (value instanceof Capture) {
-		// 	x = x.concat(value.value)
-		// }
-		// else if (typeof value === 'object' && value.splice !== undefined) {
-		// 	for (var i = 0; i < value.length; i++) {
-		// 		if (value[i] instanceof Capture) {
-		// 			x = x.concat(value[i].value)
-		// 		}
-		// 		else {
-		// 			x.push(value[i])
-		// 		}
-		// 	}
-		// }
-		// else {
-		// 	x.push(value)
-		// }
-
-		// this.value = x
-		
-		
-		var v = value
+		let v = value
 		if (value instanceof Capture) { 
 			v = value.value
 		}
-		
+
 		if (v.splice === undefined) { 
 			v = [v]
 		}
-		
+
 		for (var i = 0; i < v.length; i++) {
 			if (v[i] instanceof Capture) {
-				x = x.concat(v[i].value)
+				arr = arr.concat(v[i].value)
 			}
 			else {
-				x.push(v[i])
+				arr.push(v[i])
 			}
 		}
-		
-		this.value = x
+
+		this.value = arr
 	}
 }
 
