c6eea80c6e45096420b4a62a323c43950f3d2b00
cshepp <me@codyshepp.com>
2015-10-10 09:21:49 -0400

    Initial refactor of Capture

--- src/Kasai.Compare/Capture.ts
+++ src/Kasai.Compare/Capture.ts
@@ -7,23 +7,44 @@ export class Capture {
 
 		let x = []
 
-		if (value instanceof Capture) {
-			x = x.concat(value.value)
+		// if (value instanceof Capture) {
+		// 	x = x.concat(value.value)
+		// }
+		// else if (typeof value === 'object' && value.splice !== undefined) {
+		// 	for (var i = 0; i < value.length; i++) {
+		// 		if (value[i] instanceof Capture) {
+		// 			x = x.concat(value[i].value)
+		// 		}
+		// 		else {
+		// 			x.push(value[i])
+		// 		}
+		// 	}
+		// }
+		// else {
+		// 	x.push(value)
+		// }
+
+		// this.value = x
+		
+		
+		var v = value
+		if (value instanceof Capture) { 
+			v = value.value
 		}
-		else if (typeof value === 'object' && value.splice !== undefined) {
-			for (var i = 0; i < value.length; i++) {
-				if (value[i] instanceof Capture) {
-					x = x.concat(value[i].value)
-				}
-				else {
-					x.push(value[i])
-				}
+		
+		if (v.splice === undefined) { 
+			v = [v]
+		}
+		
+		for (var i = 0; i < v.length; i++) {
+			if (v[i] instanceof Capture) {
+				x = x.concat(v[i].value)
+			}
+			else {
+				x.push(v[i])
 			}
 		}
-		else {
-			x.push(value)
-		}
-
+		
 		this.value = x
 	}
 }
--- target/Kasai.Compare/Capture.js
+++ target/Kasai.Compare/Capture.js
@@ -1,21 +1,37 @@
 var Capture = (function () {
     function Capture(value) {
         var x = [];
+        // if (value instanceof Capture) {
+        // 	x = x.concat(value.value)
+        // }
+        // else if (typeof value === 'object' && value.splice !== undefined) {
+        // 	for (var i = 0; i < value.length; i++) {
+        // 		if (value[i] instanceof Capture) {
+        // 			x = x.concat(value[i].value)
+        // 		}
+        // 		else {
+        // 			x.push(value[i])
+        // 		}
+        // 	}
+        // }
+        // else {
+        // 	x.push(value)
+        // }
+        // this.value = x
+        var v = value;
         if (value instanceof Capture) {
-            x = x.concat(value.value);
+            v = value.value;
         }
-        else if (typeof value === 'object' && value.splice !== undefined) {
-            for (var i = 0; i < value.length; i++) {
-                if (value[i] instanceof Capture) {
-                    x = x.concat(value[i].value);
-                }
-                else {
-                    x.push(value[i]);
-                }
+        if (v.splice === undefined) {
+            v = [v];
+        }
+        for (var i = 0; i < v.length; i++) {
+            if (v[i] instanceof Capture) {
+                x = x.concat(v[i].value);
             }
-        }
-        else {
-            x.push(value);
+            else {
+                x.push(v[i]);
+            }
         }
         this.value = x;
     }
