ac13c54069edefc018236487165f0db23c6cde95
cshepp <me@codyshepp.com>
2015-10-17 11:05:38 -0400

    Export 'when' from top level module

--- src/kasai.ts
+++ src/kasai.ts
@@ -2,9 +2,11 @@
 import {match as _match} from './Kasai.Match/Match'
 import {_ as x, typeOf as _typeOf, instanceOf as _instanceOf} from './Kasai.Compare/Helpers'
 import {$ as y, Capture} from './Kasai.Compare/Capture' 
+import {when as z} from './Kasai.Compare/When'
 
 export var match = _match
 export var _ = x
 export var typeOf = _typeOf
 export var instanceOf = _instanceOf
 export var $ = y
+export var when = z
--- target/kasai.js
+++ target/kasai.js
@@ -1,8 +1,10 @@
 var Match_1 = require('./Kasai.Match/Match');
 var Helpers_1 = require('./Kasai.Compare/Helpers');
 var Capture_1 = require('./Kasai.Compare/Capture');
+var When_1 = require('./Kasai.Compare/When');
 exports.match = Match_1.match;
 exports._ = Helpers_1._;
 exports.typeOf = Helpers_1.typeOf;
 exports.instanceOf = Helpers_1.instanceOf;
 exports.$ = Capture_1.$;
+exports.when = When_1.when;
